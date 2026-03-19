4946dec226c6682f15dabcfdfedd8c2c566c5fc6
ayushiawasthi1418-png <ayushiawasthi1418@gmail.com>
2026-03-19 19:40:06 +0530

    ok

--- Core-Java/src/com/rays/basic/SecLarNoArr.java
+++ Core-Java/src/com/rays/basic/SecLarNoArr.java
@@ -1,5 +1,22 @@
 package com.rays.basic;
 
-public class SecLarNoArr {
+public class SecLarNoArr {public static void main(String[] args) {
+	int a[] = {5, 10, 15, 20, 25};
 
+    int max = a[0];
+    int secondMax = a[0];
+
+    for (int i = 0; i < a.length; i++) {
+        if (a[i] > max) {
+            secondMax = max;
+            max = a[i];
+        } else if (a[i] > secondMax && a[i] != max) {
+            secondMax = a[i];
+        }
+    }
+
+    System.out.println("Second largest number is: " + secondMax);
 }
+	
+}
+
