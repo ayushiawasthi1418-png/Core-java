e52a1b9656ed70c8027dec6a456a0ab96461ce70
ayushiawasthi1418-png <ayushiawasthi1418@gmail.com>
2026-03-19 19:27:53 +0530

    ok

--- Core-Java/src/com/rays/basic/Exercise.java
+++ Core-Java/src/com/rays/basic/Exercise.java
@@ -1,23 +1,76 @@
 package com.rays.basic;
 public class Exercise {
-	public static void main(String[] args) {
-		//question1
-//		int fact=1;
-//		for(int i=1; i<10; i++) {
-//			fact=fact*i;
-//			System.out.println(fact );
+//	public static void main(String[] args) {
+////	//	question4
+////		int fact=1;
+////		for(int i=1; i<10; i++) {
+////			fact=fact*i;
+////			System.out.println(fact );
+////		}
+////		//question7
+////		for(int i=100; i<=200;i++) {
+////			System.out.println(i);
+////			if(i%7==0) {
+////				System.out.println(i+"i is divisible by 7");
+////			}
+////		}
+////		//question5
+//         int i=231     ,rev=0;
+//         while(i!=0) {
+//        	 int num=i%10;   
+//        	 rev=rev*10+num;
+//        	 i=i/10;
+//         }
+//         System.out.println("reversed number is "+ rev);
+//         
+//         
+//         
+////			
+////		}
+//		
+//	}
+//	//question6
+//	public static void main(String[] args) {
+//		int a=0;
+//		int a1=1;
+//		while(a<=100) {
+//			System.out.println(a);
+//			int sum=a+a1;
+//			a=a1;
+//			a1=sum;
 //		}
-		//question7
-		for(int i=100; i<=200;i++) {
-			System.out.println(i);
-			if(i%7==0) {
-				System.out.println("i is divisible by 7");
-			}else {
-				System.out.println("i is not divisible by 100");
-			}
+//		
+//	}
+//	//question9
+//	public static void main(String[] args) {
+//		for(int i=1; i<=4; i++) {
+//			for(int j=1; j<=i; j++) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+//	}
+//	public static void main(String[] args) {
+//		for(int i=4; i>=1; i--) {
+//			for(int j=1; j<=i; j++) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+//	}
+	public static void main(String[] args) {
+		int i=153;
+		while(i!=0) {
+			int num=i%10;
+			int rev=num*1000;
+			int arm=arm+rev;
+			System.out.println(arm);
+			
+			
 		}
-		
 	}
 
 }
+//
+
 
