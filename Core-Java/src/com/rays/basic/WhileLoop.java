ea8827ffe4538dfd2aa912f2f9043b551d6e71e6
ayushiawasthi1418-png <ayushiawasthi1418@gmail.com>
2026-05-29 13:07:57 +0530

    done

--- Core-Java/src/com/rays/basic/WhileLoop.java
+++
@@ -1,5 +0,0 @@
-package com.rays.basic;
-
-public class WhileLoop {
-
-}
--- Core-Java/src/com/rays/basic/ayushi.java
+++
@@ -1,9 +0,0 @@
-package com.rays.basic;
-
-public class ayushi {
-	public static void main(String[] args) {
-		System.out.println("hello java");
-	 
-	}
-
-}
